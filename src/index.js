d69d50b467e7ce14809c4998ed49e297472bb0ee
Caroline Eubanks <cmeubanks@gmail.com>
2021-04-22 13:50:10 -0600

    fetch is working and connected to index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
-
+import {getData} from './api'
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
 console.log('This is the JavaScript entry file - your code begins here.');
+
+const travelerData = document.querySelector('#travelerData')
+
+window.addEventListener('load', loadData)
+
+function loadData () {
+  getData('travelers')
+  .then(response => travelerData.innerText = response.travelers[0].id)
+}
