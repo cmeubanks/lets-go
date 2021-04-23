b75a4a89ea6fb574b3faf68fa8fede3fd8f9df6f
Caroline Eubanks <cmeubanks@gmail.com>
2021-04-23 14:30:46 -0600

    set up trip test data

--- src/Destinations.js
+++ src/Destinations.js
@@ -6,9 +6,6 @@ class Destinations {
   constructor(destinationData) {
     this.destinations = destinationData;
   }
-
-
-  //method to get cost of all a user's trips (takes userID parameter)
 }
 
 
--- src/Traveler.js
+++ src/Traveler.js
@@ -5,11 +5,26 @@ class Traveler {
   this.travelerType = travelerData.travelerType;
   this.allTrips = this.tripData;
   this.totalCostofTrips = tripCost;
+  this.pendingTrips = [];
   }
 
+
+  showPastTrips(todaysdate) {
+    //needs day.js
+    this.allTrips.filter(trip => trip.date < todaysdate)
+  }
+
+  showFutureTrips(todaysdate) {
+    this.allTrips.filter(trip => trip.date > todaysdate)
+  }
+
+  showPresentTrips(todaysdate) {
+    // const date = todaysdate.split('/')
+    // const endDate = date[1] +
+  }
 //makeTripRequest
 
-//methodToUpdateTripCost if request is made?
+
 }
 
 
