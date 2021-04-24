f909c7bb03e029994dcf533f0350d1a2d8020f54
Caroline Eubanks <cmeubanks@gmail.com>
2021-04-24 15:43:18 -0600

    updated traveler methods

--- src/Traveler.js
+++ src/Traveler.js
@@ -28,24 +28,24 @@ class Traveler {
 
   showFutureTrips(todaysDate) {
     const futureTrips = this.allTrips.filter(trip => dayjs(trip.date).isAfter(todaysDate))
-    this.future.push(futureTrips)
+
+    const result = futureTrips.map(trip => ({...trip, 'destination': this.destinations.filter(place => trip.destinationID === place.id)}))
+
+    result.forEach(trip => this.future.push(trip))
   }
 
   showPresentTrips(todaysDate) {
-    // console.log(dayjs("2021/02/09").add(8, 'day'))
-    // console.log(this.allTrips)
 
     const presentTrips = this.allTrips.filter(trip => {
       const endDate = dayjs(trip.date).add(trip.duration, 'day')
       return dayjs(todaysDate).isBetween(trip.date, endDate, null, '[]')
     })
-    console.log("present trip", presentTrips)
-    this.present.push(presentTrips)
-    //get a new object with date range
-    // this.allTrips.map(trip => {...trip, 'tripDates': })
-    //use dayJS to get dates between that check new array
+
+    const result = presentTrips.map(trip => ({...trip, 'destination': this.destinations.filter(place => trip.destinationID === place.id)}))
+
+    result.forEach(trip => this.present.push(trip))
   }
-//makeTripRequest
+
 
 
 }
