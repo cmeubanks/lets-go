42a1c4eba76c8727c9291f76a6b79dc1fde128a6
Caroline Eubanks <cmeubanks@gmail.com>
2021-04-25 11:11:17 -0600

    added promise to correct fetch timing

--- src/scripts.js
+++ src/scripts.js
@@ -25,25 +25,53 @@ formBtn.addEventListener('click', loadForm)
 
 
 function loadData () {
-  getData('destinations')
-    .then(response => destinationsInfo = response)
-  getData('trips')
-    .then(response => tripsInfo = response)
-  getData('travelers/1')
-    .then(response => travelerInfo = response)
-    .then(response => {
-      console.log("array holder", destinationsInfo)
-      destinations = new Destinations(destinationsInfo.destinations)
-      console.log("destination class", destinations) })
-    .then(response => {
-      trip = new Trip(destinationsInfo.destinations, tripsInfo.trips)
-      traveler = new Traveler(travelerInfo, trip.getTravelersTrips(2), trip.travelerTotalSpentInYear(2,"2021/01/09"), destinationsInfo.destinations)
-      traveler.showPastTrips("2021/01/09");
-      traveler.showFutureTrips("2021/01/09");
-      traveler.showPresentTrips("2021/01/09");
+  Promise.all([getData('destinations'), getData('trips'), getData('travelers/2')])
+    .then(data => {
+      destinations = new Destinations(data[0].destinations)
+      trip = new Trip(data[0].destinations, data[1].trips)
+      traveler = new Traveler(data[2], trip.getTravelersTrips(2), trip.travelerTotalSpentInYear(2, "2021/01/09"), data[0].destinations)
+      showTrips("2021/01/09")
     })
-  }
 
+
+}
+  //   .then(response => travelerInfo = response)
+  //   .then(response => {
+  //     console.log("array holder", destinationsInfo)
+  //     destinations = new Destinations(destinationsInfo.destinations)
+  //     console.log("destination class", destinations) })
+  //   .then(response => {
+  //     trip = new Trip(destinationsInfo.destinations, tripsInfo.trips)
+  //     traveler = new Traveler(travelerInfo, trip.getTravelersTrips(2), trip.travelerTotalSpentInYear(2,"2021/01/09"), destinationsInfo.destinations)
+  //     traveler.showPastTrips("2021/01/09");
+  //     traveler.showFutureTrips("2021/01/09");
+  //     traveler.showPresentTrips("2021/01/09");
+  //   })
+  // }
+
+  // getData('destinations')
+  //   .then(response => destinationsInfo = response)
+  // getData('trips')
+  //   .then(response => tripsInfo = response)
+  // getData('travelers/1')
+  //   .then(response => travelerInfo = response)
+  //   .then(response => {
+  //     console.log("array holder", destinationsInfo)
+  //     destinations = new Destinations(destinationsInfo.destinations)
+  //     console.log("destination class", destinations) })
+  //   .then(response => {
+  //     trip = new Trip(destinationsInfo.destinations, tripsInfo.trips)
+  //     traveler = new Traveler(travelerInfo, trip.getTravelersTrips(2), trip.travelerTotalSpentInYear(2,"2021/01/09"), destinationsInfo.destinations)
+  //     traveler.showPastTrips("2021/01/09");
+  //     traveler.showFutureTrips("2021/01/09");
+  //     traveler.showPresentTrips("2021/01/09");
+  //   })
+
+function showTrips(date) {
+  traveler.showPastTrips(date);
+  traveler.showFutureTrips(date);
+  traveler.showPresentTrips(date);
+}
 
 function displayTrips(event) {
   // event.preventDefault()
